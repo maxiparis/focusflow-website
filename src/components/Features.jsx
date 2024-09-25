8b0cf32c1ead03d8e4a176a0c174631990a05b1f
Maximiliano Paris <mparis@hotmail.cl>
2024-09-25 13:49:24 -0600

    fix: word repetition effortlessly

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,7 @@ import { Card, Container, Row, Col } from 'react-bootstrap';
 
 function Features() {
     const featureList = [
-        { title: 'Focus Sessions', description: 'Initiate your sessions effortlessly, allowing you to focus more on work than on planning.', icon: 'bi bi-alarm' },
+        { title: 'Focus Sessions', description: 'Initiate your sessions easily, allowing you to focus more on work than on planning.', icon: 'bi bi-alarm' },
         { title: 'Time Tracking', description: 'Effortlessly monitor the time left on your tasks to stay focused and productive.', icon: 'bi bi-clock' },
         { title: 'Reminders', description: 'Get notified when it\'s time to begin and wrap up your tasks.', icon: 'bi bi-bell' },
         { title: 'Quantify your Progress', description: 'Get a detailed report on the time you\'ve saved by using this app.', icon: 'bi bi-graph-up' }
