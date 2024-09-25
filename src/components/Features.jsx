8ff77ed33cdec12fc9a5678735cecca951440fdb
Maximiliano Paris <mparis@hotmail.cl>
2024-09-25 14:09:07 -0600

    feat: more features and better icons

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,10 +2,12 @@ import { Card, Container, Row, Col } from 'react-bootstrap';
 
 function Features() {
     const featureList = [
-        { title: 'Focus Sessions', description: 'Initiate your sessions easily, allowing you to focus more on work than on planning.', icon: 'bi bi-alarm' },
-        { title: 'Time Tracking', description: 'Effortlessly monitor the time left on your tasks to stay focused and productive.', icon: 'bi bi-clock' },
+        { title: 'Focus Sessions', description: 'Initiate your sessions easily, allowing you to focus more on work than on planning.', icon: 'bi bi-calendar2-check' },
+        { title: 'Time Tracking', description: 'Effortlessly monitor the time left on your tasks to stay focused and productive.', icon: 'bi bi-hourglass-split' },
         { title: 'Reminders', description: 'Get notified when it\'s time to begin and wrap up your tasks.', icon: 'bi bi-bell' },
-        { title: 'Quantify your Progress', description: 'Get a detailed report on the time you\'ve saved by using this app.', icon: 'bi bi-graph-up' }
+        { title: 'Precise Finish Time Predictions', description: 'Get precise predictions on your completion time, allowing you to plan your day more effectively.', icon: 'bi bi-watch' },
+        { title: 'Conscious Timing', description: 'Stay informed about any time overruns on your tasks by knowing exactly how much longer your focus session will extend.', icon: 'bi bi-person-check' },
+        { title: 'Quantify your Progress', description: 'Get a detailed report on the time you\'ve saved by using this app.', icon: 'bi bi-graph-up-arrow' }
     ];
 
     return (
