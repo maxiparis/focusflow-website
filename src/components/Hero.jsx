934bea13c786b7dace6bf7f3626053bc1b72739b
Maximiliano Paris <mparis@hotmail.cl>
2024-09-25 21:26:24 -0600

    feat: vertical space in image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,10 @@ function Hero() {
     return (
         <Container className="hero-section" style={{ padding: '50px 0', textAlign: 'center', backgroundColor: '#f5f5f5' }}>
             <Row>
-                <Col md={6} className="align-self-center">
+                <Col md={6} className="align-self-center mb-4 mb-md-0">
                     <h1 className={"display-1"}>FocusFlow</h1>
                     <h1 className={"display-4 p-3"}>Boost Your Productivity with Our App</h1>
 
-                    {/*TOOD: insert link to survey*/}
                     <Button variant="success" size="lg" href="https://forms.gle/ukWuVMNYGxAmk3Uk7" >
                         <i className="bi bi-pencil"></i> Participate in our survey
                     </Button>
@@ -18,7 +17,8 @@ function Hero() {
                         src="public/demo.jpg"
                         alt="App Screenshot"
                         style={{ width: '350px', height: 'auto'}}
-                        fluid />
+                        fluid
+                    />
                 </Col>
             </Row>
         </Container>
