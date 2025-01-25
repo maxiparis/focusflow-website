400e3ee650e97113bc108e02f67014c4cf41caa4
Maximiliano Paris <mparis@hotmail.cl>
2025-01-24 23:20:22 -0700

    fix: year copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function OurFooter() {
             <Container className={"bg-body-secondary"}>
                 <Row>
                     <Col className="text-center mt-3">
-                        <p className="mb-0">&copy; 2024 FocusFlow. All rights reserved.</p>
+                        <p className="mb-0">&copy; 2025 FocusFlow. All rights reserved.</p>
                     </Col>
                 </Row>
             </Container>
